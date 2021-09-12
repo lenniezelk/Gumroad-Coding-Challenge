e0a52d03fe677c30dae1abafba8912b92e0f0360
Leonard Kioi Kinyanjui <lenny.kinyanjui.lk@gmail.com>
2021-09-13 00:49:30 +0300

    add firebase related tools for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  purge: {
+    enabled: true,
+    content: [
+      "./src/**/*.html",
+      "./src/**/*.js",
+      "./src/**/*.ts",
+      "./src/**/*.tsx",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
