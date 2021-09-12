46e787ca8d779e533f2861dbb7d34e238e1b94e8
Leonard Kioi Kinyanjui <lenny.kinyanjui.lk@gmail.com>
2021-09-12 20:27:19 +0300

    create a stub App that uses react to fetch and render a product

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,78 @@
-export const App = () => <div>Webpack HMR Example</div>;
+import "tailwindcss/tailwind.css";
+import { initializeApp, FirebaseApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  setDoc,
+  Firestore,
+} from "firebase/firestore";
+import { Product, Rating } from "./product.types";
+import { renderProduct } from "./products";
+import { Subject } from "rxjs";
+import { useEffect, useRef, useState } from "react";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDc8RzRYtxlIekQyPBu--0qwKombEXN9W4",
+  authDomain: "gumroad-coding-challenge.firebaseapp.com",
+  projectId: "gumroad-coding-challenge",
+  storageBucket: "gumroad-coding-challenge.appspot.com",
+  messagingSenderId: "853818007743",
+  appId: "1:853818007743:web:b243172d8012b48467a60a",
+  measurementId: "G-LXCP9GCLW8",
+};
+
+export const App = () => {
+  const app = useRef<FirebaseApp>(null);
+  const db = useRef<Firestore>(null);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  // fetch products and ratings
+  const fetchDocs = async () => {
+    const productSnapshot = await getDocs(collection(db.current, "products"));
+    productSnapshot.forEach(async (doc) => {
+      const product: Product = doc.data() as Product;
+      product.id = doc.id;
+
+      // fetch product rating
+      const ratingSnapshot = await getDocs(
+        collection(db.current, `products/${product.id}/ratings`)
+      );
+
+      const ratings: Rating[] = [];
+
+      ratingSnapshot.forEach((doc) => {
+        const rating = doc.data() as Rating;
+        rating.id = doc.id;
+        ratings.push(rating);
+      });
+
+      product.ratings = ratings;
+      setProducts([...products, product]);
+    });
+  };
+
+  useEffect(() => {
+    app.current = initializeApp(firebaseConfig);
+    db.current = getFirestore(app.current);
+    fetchDocs();
+  }, []);
+
+  //   const addReview = async (path: string, reviewText: string) => {
+  //     const ratingDocRef = doc(collection(db, path));
+  //     await setDoc(ratingDocRef, { value: 5, text: reviewText });
+  //   };
+
+  // add products to DOM
+  //   const renderProducts = () => {
+  //     const productItem = products.map((product) => {
+  //       const addProductReview = (reviewText: string) =>
+  //         addReview(`products/${product.id}/ratings`, reviewText);
+  //       return renderProduct(product, addProductReview);
+  //     });
+  //     const productsNode = document.getElementById("products");
+  //     productsNode.append(...productItem);
+  //   };
+  return <div>{products.map((product) => product.name)}</div>;
+};
