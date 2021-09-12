287959c1aede4b4178259cd1557a2420c5451700
Leonard Kioi Kinyanjui <lenny.kinyanjui.lk@gmail.com>
2021-09-12 11:15:56 +0300

    add  firebase

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import "tailwindcss/tailwind.css";
 import { join } from "lodash";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDc8RzRYtxlIekQyPBu--0qwKombEXN9W4",
+  authDomain: "gumroad-coding-challenge.firebaseapp.com",
+  projectId: "gumroad-coding-challenge",
+  storageBucket: "gumroad-coding-challenge.appspot.com",
+  messagingSenderId: "853818007743",
+  appId: "1:853818007743:web:b243172d8012b48467a60a",
+  measurementId: "G-LXCP9GCLW8",
+};
 
 function component() {
   const element = document.createElement("div");
@@ -11,3 +22,4 @@ function component() {
 }
 
 document.body.appendChild(component());
+initializeApp(firebaseConfig);
